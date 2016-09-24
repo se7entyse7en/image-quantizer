2b9c82647c027e9a67e844b0f061be7273e8be94
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2016-09-24 20:51:39 +0200

    Splitted the concrete quantizer into a separeted module

--- image_quantizer/quantizer.py
+++ image_quantizer/quantizer.py
@@ -6,15 +6,12 @@
 from itertools import groupby
 from operator import attrgetter
 
-import numpy as np
-
 import scipy.misc
 
-from sklearn import cluster
-from sklearn.metrics import pairwise_distances_argmin
-from sklearn.utils import shuffle
-
 import matplotlib.pyplot as plt
+
+from image_quantizer.concrete_quantizer import KMeansQuantizer
+from image_quantizer.concrete_quantizer import RandomQuantizer
 
 
 def compare(*quantized_images):
@@ -105,8 +102,9 @@ class ImageQuantizer(object):
     """The image color quantizer
 
     Depending on the method to use this class uses the proper concrete
-    quantizer (subclass of :class:`IConcreteQuantizer`) by calling its
-    `quantize` method.
+    quantizer (subclass of
+    :class:`image_quantizer.concrete_quantizer.IConcreteQuantizer`) by calling
+    its `quantize` method.
 
     """
     method_choices = {
@@ -179,67 +177,3 @@ class ImageQuantizer(object):
                           image_filename=image_filename)
             for qp in quantization_params
         ]
-
-
-class IConcreteQuantizer(object):
-    """Interface of a concrete quantizer
-
-    Each subclass must implement the :meth:`IConcreteQuantizer.quantize`
-    method.
-
-    """
-    @classmethod
-    def quantize(cls, raster, n_colors, **kwargs):
-        """Quantizes the given raster using the given parameters
-
-        :param :class:`numpy.ndarray` raster: the raster of the image in RGB
-                                              with values normalized in in
-                                              [0, 1] and shape (width, height,
-                                              3).
-        :param int n_colors: the number of colors to use for the color
-                             quantization.
-
-        """
-        raise NotImplementedError()
-
-    @classmethod
-    def _recreate_image(cls, palette, labels, width, height):
-        return np.reshape(palette[labels], (width, height, palette.shape[1]))
-
-
-class RandomQuantizer(IConcreteQuantizer):
-    """Concrete quantizer that randomly selects the color palette"""
-
-    @classmethod
-    def quantize(cls, raster, n_colors, **kwargs):
-        width, height, depth = raster.shape
-        reshaped_raster = np.reshape(raster, (width * height, depth))
-
-        palette = shuffle(reshaped_raster)[:n_colors]
-        labels = pairwise_distances_argmin(reshaped_raster, palette)
-
-        quantized_raster = cls._recreate_image(palette, labels, width, height)
-
-        return quantized_raster
-
-
-class KMeansQuantizer(IConcreteQuantizer):
-    """Concrete quantizer that selects the color palette using K-means
-
-    The K-means algorithm is run over the pixel of the images using `k`
-    clusters where `k` is the number of colors. At the end of the clustering
-    the color palette will be composed by the centroids of model.
-
-    """
-    @classmethod
-    def quantize(cls, raster, n_colors, **kwargs):
-        width, height, depth = raster.shape
-        reshaped_raster = np.reshape(raster, (width * height, depth))
-
-        model = cluster.KMeans(n_clusters=n_colors)
-        labels = model.fit_predict(reshaped_raster)
-        palette = model.cluster_centers_
-
-        quantized_raster = cls._recreate_image(palette, labels, width, height)
-
-        return quantized_raster
